ee6c296b21a81caa0b2acbfa91ac7bb69b1e92c4
yonathanwemerge <133159632+yonathanwemerge@users.noreply.github.com>
2023-05-11 15:19:56 -0600

    Feature/register researcher (#1)
    * forge install: openzeppelin-contracts
    
    v4.8.3
    
    * remove gitmodule
    
    * forge install: openzeppelin-contracts
    
    v4.8.3
    
    * add oppenzeplin-contracts
    
    * add security
    
    * Update main.sol
    
    ---------
    
    Co-authored-by: Yonathan Cruz <yonathancruz2019@gmail.com>

--- fevm-foundry-kit/src/main.sol
+++ fevm-foundry-kit/src/main.sol
@@ -1,6 +1,39 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity 0.8.13
+pragma solidity 0.8.17;
 
-contract EHR {
+import { ReentrancyGuard } from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
+contract EHR is Ownable, ReentrancyGuard {
+    /// @notice Pause the contract
+    bool public paused;
+    /// @notice Fee to register as researcher
+    uint256 public researcherFee = 0.1 ether;
+    /// @notice Identifyer of researcher
+    mapping(address => bytes32) public researchers;
+    /// @notice Researcher has registered
+    event ApplicantRegistered(address indexed researcher, bytes32 identifier);
+
+    /// @notice Allows to anyone else can register as Researcher only need to pay the fee
+    function RegisterResearcher(
+        bytes32 _identifier
+    ) public payable nonReentrant {
+        require(!paused, "EHR: Contract is paused");
+        require(msg.value > 0, "EHR: Not enough msg.value");
+        require(msg.value > researcherFee, "EHR: Incorrect balance sent");
+        address researcher = _msgSender();
+        // Save researcher identifier
+        researchers[researcher] = _identifier;
+        emit ApplicantRegistered(researcher, _identifier);
+    }
+
+    /// @notice Allows owner pause the contract
+    function setPause() external onlyOwner {
+        paused = !paused;
+    }
+
+    /// @notice Allows owner change the researcherFee
+    function setResearcherFee(uint256 _newValue) external onlyOwner {
+        researcherFee = _newValue;
+    }
 }
