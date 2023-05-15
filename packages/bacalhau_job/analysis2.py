499ac2fa1ce440edb12025a0a05a6751b7a35278
Alexander Potapov <al.potapov@hotmail.com>
2023-05-15 20:15:26 +0200

    Script that will be executed by Bacalhau

---
+++ packages/bacalhau_job/analysis2.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# Set seed for reproducibility
+np.random.seed(0)
+
+# Create a dataframe with sleep hours and depression score
+sleep_less = np.random.normal(loc=6, scale=1.5, size=500)
+depression_less = np.random.normal(loc=60, scale=10, size=500)
+
+sleep_more = np.random.normal(loc=9, scale=1.5, size=500)
+depression_more = np.random.normal(loc=40, scale=10, size=500)
+
+sleep = np.concatenate([sleep_less, sleep_more])
+depression = np.concatenate([depression_less, depression_more])
+group = ['<8 hours']*500 + ['>8 hours']*500
+
+data = pd.DataFrame({'Sleep Duration': sleep, 'Depression Score': depression, 'Group': group})
+
+# Create the scatter plot
+plt.figure(figsize=(10, 6))
+sns.scatterplot(x='Sleep Duration', y='Depression Score', hue='Group', data=data)
+plt.title('Correlation between Sleep Duration and Depression')
+plt.show()
